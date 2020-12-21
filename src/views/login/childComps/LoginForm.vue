189b7e26bd0b2003280cf3e17b5956d80adb67e1
Lee <5389233+zhu_jin_lan@user.noreply.gitee.com>
2020-12-21 15:59:11 +0800

    用户登录-页面布局-使用form表单

---
+++ src/views/login/childComps/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-form"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped lang="less">
+.login-form {
+}
+</style>
